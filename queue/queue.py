3c4a27504ba77fec75c4d558085440b93cbfc43f
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-06-10 18:36:43 -0400

    Finally find missing return for last test to pass

--- queue/queue.py
+++ queue/queue.py
@@ -1,31 +1,38 @@
 class Node:
-    def __init__(self, value, next):
-        self.value = value # 3 ==>    3, 5
-        self.next = next   # None ==> 5, none
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
 
 class LinkedList:
     def __init__(self):
-        self.head = None # 3
-        self.tail = None # 3
-                          # 5
+        self.head = None
+        self.tail = None
+
     def add_to_back(self, item):
           
-        added_node = Node(item) # new_node.value = 5
-                              # new_node.next = none
-        if not self.tail:
+        added_node = Node(item)
+
+        if not self.tail and not self.head:
             self.head = added_node
             self.tail = added_node
         else:
-            self.tail.next = added_node # next = 5
-            self.tail = self.tail.next # tail = 5
+            self.tail.next = added_node
+            self.tail = added_node
     
     def remove_from_front(self):
-        if self.head:
-            removed_node = self.head
-            self.head = self.head.next
-            return removed_node.value
-        else:
-            return "Cannot dequeue, queue is empty"
+        if not self.head and not self.tail:
+            return None
+
+        if self.head is self.tail:
+            front_node = self.head
+            self.head = None
+            self.tail = None
+            return front_node.value
+
+        front_node = self.head # 3
+        self.head = front_node.next
+        return front_node.value
+
 
 class Queue:
   def __init__(self):
@@ -37,9 +44,10 @@ class Queue:
     self.size += 1
   
   def dequeue(self):
-    self.storage.remove_from_front()
     if self.size > 0:
         self.size -= 1
+
+    return self.storage.remove_from_front()
 
   def len(self):
       return self.size
