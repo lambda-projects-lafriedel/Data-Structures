fa198de7d4021c693b298a5e3b454bede185028c
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-06-10 16:37:29 -0400

    Scaffolding for queue inplementation

--- queue/queue.py
+++ queue/queue.py
@@ -1,9 +1,26 @@
+class Node:
+    def __init__(self, value, next):
+        self.value = value
+        self.next = next
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+    
+    def add_to_back(self, item):
+        pass
+    
+    def remove_from_front(self):
+        pass
+    
+    def get_length(self):
+        pass
+
 class Queue:
   def __init__(self):
     self.size = 0
-    # what data structure should we
-    # use to store queue elements?
-    self.storage = 
+    self.storage = LinkedList()
 
   def enqueue(self, item):
     pass
