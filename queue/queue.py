4f76c66e5c131b6adf7f94ad5cff7f7022b5c644
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-06-10 17:03:56 -0400

    Implement add_to_back for enqueueing

--- queue/queue.py
+++ queue/queue.py
@@ -1,15 +1,26 @@
 class Node:
     def __init__(self, value, next):
-        self.value = value
-        self.next = next
+        self.value = value # 3 ==>    3, 5
+        self.next = next   # None ==> 5, none
+    
+    def change_next(self, new_next)
+        self.next = new_next
 
 class LinkedList:
     def __init__(self):
-        self.head = None
-        self.tail = None
-    
+        self.head = None # 3
+        self.tail = None # 3
+                          # 5
     def add_to_back(self, item):
-        pass
+          
+        new_node = Node(item) # new_node.value = 5
+                              # new_node.next = none
+        if not self.head and not self.tail:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node # next = 5
+            self.tail = self.tail.next # tail = 5
     
     def remove_from_front(self):
         pass
