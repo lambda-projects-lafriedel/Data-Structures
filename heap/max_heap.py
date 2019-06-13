63042fc38499c150b045e523e587e9ec7b275c2b
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-06-13 16:27:15 -0400

    Pass all tests for max_heap

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -11,12 +11,20 @@ class Heap:
 
   def delete(self):
     # store ref to first element, to be returned
-    max_elem = self.storage.pop(0)
-    # move last num in array to first index and pop the last value off
-    last_elem = self.storage.pop()
-    self.storage.insert(0, last_elem)
-    # call sift_down
-    self._sift_down(0)
+    print("BEFORE IF:", self.storage)
+    print("SIZE:", self.get_size())
+    if self.get_size() > 0:
+        max_elem = self.storage.pop(0)
+        #move last num in array to first index and pop the last value off
+        print("AFTER MAX ELEM POP:", self.storage)
+        print("SIZE:", self.get_size())
+        if self.get_size() > 0:
+            last_elem = self.storage.pop()
+            self.storage.insert(0, last_elem)
+            print("AFTER INSERT:", self.storage)
+        # call sift_down
+            self._sift_down(0)
+        return max_elem
 
   def get_max(self):
     # return first element
@@ -40,26 +48,32 @@ class Heap:
 
   def _sift_down(self, index):
       # while index is less than the length of self.storage
+      # 
       while index < len(self.storage):
+          # print("IDX:", index)
       # get the left and right child indices
           l_index = 2 * index + 1
-          r_index = 2 * index + 2
-          # if l and r indices are equal, swap with right index
-          if self.storage[l_index] == self.storage[r_index]:
-              self.storage[index], self.storage[r_index] = self.storage[r_index], self.storage[index]
-              # set index as the right index, since that's index's new loc
-              index = r_index
-          # if curr index is less than either or both of its children
-          elif self.storage[index] < self.storage[l_index] or self.storage[index] < self.storage[r_index]:
-              # if right index is greater than left index, swap right index and curr index
-              if self.storage[r_index] > self.storage[l_index]:
-                   self.storage[index], self.storage[r_index] = self.storage[r_index], self.storage[index]
-                   # set index as right index, since that's index's new loc
-                   index = r_index
-              # else swap left index and curr index
+          r_index = 2 * index + 2 if l_index + 1 < len(self.storage) else l_index
+          # print("L AND R:", l_index, r_index)
+          if l_index < len(self.storage) and r_index < len(self.storage):
+              # if l and r indices are equal, swap with right index
+              if self.storage[l_index] == self.storage[r_index]:
+                  self.storage[index], self.storage[r_index] = self.storage[r_index], self.storage[index]
+                  # set index as the right index, since that's index's new loc
+                  index = r_index
+              # if curr index is less than either or both of its children
+              elif self.storage[index] < self.storage[l_index] or self.storage[index] < self.storage[r_index]:
+                  # if right index is greater than left index, swap right index and curr index
+                  if self.storage[r_index] > self.storage[l_index]:
+                      self.storage[index], self.storage[r_index] = self.storage[r_index], self.storage[index]
+                      # set index as right index, since that's index's new loc
+                      index = r_index
+                  # else swap left index and curr index
+                  else:
+                      self.storage[index], self.storage[l_index] = self.storage[l_index], self.storage[index]
+                      # set index as left index, since that's index's new loc
+                      index = l_index
               else:
-                   self.storage[index], self.storage[l_index] = self.storage[l_index], self.storage[index]
-                   # set index as left index, since that's index's new loc
-                   index = l_index
+                  break
           else:
-            break
+              break
